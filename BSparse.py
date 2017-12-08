37756b62055a6a7984ff07562bd0cbcef18332bd
SamuelHLewis <samuel.h.lewis@gmail.com>
2017-12-08 14:23:45 +0000

    Added genome-wide methylation estimator

--- BSparse.py
+++ BSparse.py
@@ -21,6 +21,10 @@ if InputFile is not None:
 else:
 	print('ERROR: no input file specified')
 
+#################
+## ACTUAL CODE ##
+#################
+
 ## function to take a line from a MethyExtract output file, and return a bed-formatted line with % methylation on both strands
 def MEconverter(line):
 	Line = line.split("\t")
@@ -28,20 +32,42 @@ def MEconverter(line):
 	Start = str(int(Line[1])-1)
 	End = str(int(Line[1])+1)
 	Name = "m"+Line[2]
-	Score = str((int(Line[5])+int(Line[8]))/2)
+	# note: "Score" here is the mean of the % methylated reads on both strands
+	Score = str((((int(Line[3])/int(Line[4]))*100)+((int(Line[6])/int(Line[7]))*100))/2)
 	BedLine = Chromosome + "\t" + Start + "\t" + End + "\t" + Name + "\t" + Score
 	return(BedLine)
 
 # read input file, keep only lines with methylation called on both strands
 BothStrands = []
 for line in open(InputFile, "r"):
-	temp = line.split("\t")
-	if not "." in temp:
-		BothStrands.append(line.strip("\n"))
+	if not line.startswith("#"):
+		temp = line.split("\t")
+		if not "." in temp:
+			BothStrands.append(line.strip("\n"))
 
+# convert lines to bed format
+BedFormatLines = []
 for i in BothStrands:
-	print(i)
+        BedFormatLines.append(MEconverter(i))
 
-for i in BothStrands:
-	if not i.startswith("#"):
-		print(MEconverter(i))
+# format output bed file
+BedOutput = ""
+for i in BedFormatLines:
+	BedOutput += i + "\n"
+BedOutfile = open(InputFile.replace(".output",".bed"),"wt")
+BedOutfile.write(BedOutput)
+BedOutfile.close()
+print("Bed file written to "+InputFile.replace(".output",".bed"))
+
+# calculate genome-wide methylation level
+Ccount = 0
+MethylationTotal = 0
+for i in BedFormatLines:
+	Ccount += 1
+	MethylationTotal += float(i.split("\t")[4])
+MethylationLevel = MethylationTotal/Ccount
+print("Genome-wide methylation level = " + str(MethylationLevel))
+
+# use bedtools to intersect bed with gene models, generating mean methylation level for each gene
+# sort bedfile and gff file
+# use map to generate methylation levels for each feature (write into function)
