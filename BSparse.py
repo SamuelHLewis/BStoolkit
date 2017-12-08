756e6e38a49715dba4c757797b9a30528399d7da
SamuelHLewis <samuel.h.lewis@gmail.com>
2017-12-08 16:52:17 +0000

    Calculation of mean methylation per feature in GFF added

--- BSparse.py
+++ BSparse.py
@@ -13,6 +13,7 @@ import argparse
 ###########################
 parser = argparse.ArgumentParser(description='Read arguments')
 parser.add_argument('-i', '--infile', type=str, help='input file (should be a .output file from MethylExtract)')
+parser.add_argument('-f', '--featurefiles', type=str, help='list of feature files in gff format (comma-separated)')
 args = parser.parse_args()
 # input fasta file parsing
 InputFile = args.infile
@@ -20,10 +21,15 @@ if InputFile is not None:
 	print('Input file is ' + InputFile)
 else:
 	print('ERROR: no input file specified')
+# feature file parsing
+FeatureFiles = args.featurefiles.split(",")
+if len(FeatureFiles) > 0:
+	for i in FeatureFiles:
+		print('Feature file = ' + i)
 
-#################
-## ACTUAL CODE ##
-#################
+##########################
+## FUNCTION DEFINITIONS ##
+##########################
 
 ## function to take a line from a MethyExtract output file, and return a bed-formatted line with % methylation on both strands
 def MEconverter(line):
@@ -37,37 +43,63 @@ def MEconverter(line):
 	BedLine = Chromosome + "\t" + Start + "\t" + End + "\t" + Name + "\t" + Score
 	return(BedLine)
 
-# read input file, keep only lines with methylation called on both strands
-BothStrands = []
-for line in open(InputFile, "r"):
-	if not line.startswith("#"):
-		temp = line.split("\t")
-		if not "." in temp:
-			BothStrands.append(line.strip("\n"))
+## function to take a MethylExtract output file and output a bed file
+def MEtoBED(MEfile):
+	# read input file, keep only lines with methylation called on both strands
+	BothStrands = []
+	for line in open(MEfile, "r"):
+		if not line.startswith("#"):
+			temp = line.split("\t")
+			if not "." in temp:
+				BothStrands.append(line.strip("\n"))
 
-# convert lines to bed format
-BedFormatLines = []
-for i in BothStrands:
-        BedFormatLines.append(MEconverter(i))
+	# convert lines to bed format
+	BedFormatLines = []
+	for i in BothStrands:
+		BedFormatLines.append(MEconverter(i))
 
-# format output bed file
-BedOutput = ""
-for i in BedFormatLines:
-	BedOutput += i + "\n"
-BedOutfile = open(InputFile.replace(".output",".bed"),"wt")
-BedOutfile.write(BedOutput)
-BedOutfile.close()
-print("Bed file written to "+InputFile.replace(".output",".bed"))
+	# format output bed file
+	BedOutput = ""
+	for i in BedFormatLines:
+		BedOutput += i + "\n"
+	BedOutfile = open(MEfile.replace(".output",".bed"),"wt")
+	BedOutfile.write(BedOutput)
+	BedOutfile.close()
+	print("Bed file written to "+MEfile.replace(".output",".bed"))
+	# calculate genome-wide methylation level
+	BedFile = MEfile.replace(".output",".bed")
+	Ccount = 0
+	MethylationTotal = 0
+	for i in BedFormatLines:
+		Ccount += 1
+		MethylationTotal += float(i.split("\t")[4])
+	MethylationLevel = MethylationTotal/Ccount
+	print("Genome-wide methylation level for " + BedFile + "= " + str(MethylationLevel) + "%")
+	GenomeMethylationOutput = "Genome-wide methylation for " + BedFile + "\n" + str(MethylationLevel)
+	GenomeMethylationOutfile = open(BedFile.replace(".bed",".MethSummary"),"wt")
+	GenomeMethylationOutfile.write(GenomeMethylationOutput)
+	GenomeMethylationOutfile.close()	
+	return(MEfile.replace(".output",".bed"))
 
-# calculate genome-wide methylation level
-Ccount = 0
-MethylationTotal = 0
-for i in BedFormatLines:
-	Ccount += 1
-	MethylationTotal += float(i.split("\t")[4])
-MethylationLevel = MethylationTotal/Ccount
-print("Genome-wide methylation level = " + str(MethylationLevel))
+## function to take a bed file of methylation levels for cytosines and a gff feature file, and generate mean methylation levels for each feature
+def FeatureMeth(MethBed,GFF):
+	# sort bedfile and feature files
+	cmd = "bedtools sort -i " + MethBed + " > " + MethBed.replace(".bed",".sorted.bed")
+	subprocess.call(cmd, shell=True)
+	cmd = "bedtools sort -i " + GFF + " > " + GFF.replace(".gff",".sorted.gff")
+	subprocess.call(cmd, shell=True)
+	# generate methylation levels for each feature
+	cmd = "bedtools map -a " + GFF.replace(".gff",".sorted.gff") + " -b " + MethBed.replace(".bed",".sorted.bed") + " -c 5 -o mean > " + GFF.replace(".gff",".CG.bed")
+	subprocess.call(cmd, shell=True)
+	return()
 
-# use bedtools to intersect bed with gene models, generating mean methylation level for each gene
-# sort bedfile and gff file
-# use map to generate methylation levels for each feature (write into function)
+##################
+## ACTUAL CALLS ##
+##################
+
+# convert MethylExtract file to BED file
+InputBed = MEtoBED(MEfile=InputFile)
+
+# calculate mean methylation level for each feature in each feature file
+for i in FeatureFiles:
+	FeatureMeth(MethBed=InputBed,GFF=i)
