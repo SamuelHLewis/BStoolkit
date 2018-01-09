e2444c100a444bfe10c53313c508fee58a77a10c
SamuelHLewis <samuel.h.lewis@gmail.com>
2018-01-09 10:56:34 +0000

    Added exon start site finding

--- UpstreamFinder.py
+++ UpstreamFinder.py
@@ -6,6 +6,7 @@ import subprocess
 import shutil
 import re
 import argparse
+from collections import OrderedDict
 
 ###########################
 ## USER ARGUMENT PARSING ##
@@ -31,3 +32,26 @@ if str.isdigit(args.length):
 else:
 	print("ERROR: upstream length (-l) not specified as an integer")
 	sys.exit(0)
+
+#################
+## ACTUAL CODE ##
+#################
+# function to find start position of 1st exon for each gene - takes a GFF file, returns a dict of gene:start position pairs
+def Exon1Finder(GFF):
+	Genes = OrderedDict()
+	for line in open(GFF,"r"):
+		if not line.startswith("#"):
+			temp = line.split("\t")
+			if temp[2] == "exon":
+				Strand = temp[6]
+				Start = temp[3]
+				Name = temp[8].split(";")[1].replace("Parent=","")
+				if Name not in Genes:
+					Genes[Name] = Start
+	return(Genes)
+
+# final call
+result=Exon1Finder(GFF=InputFile)
+for i in result:
+	print(i + "\t" + result[i])
+
