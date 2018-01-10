170a92c73ba7f5fe3939adfbbffc7ae6e9f17cea
SamuelHLewis <samuel.h.lewis@gmail.com>
2018-01-10 16:32:42 +0000

    Fixed bug that created mid-model annotations

--- UpstreamFinder.py
+++ UpstreamFinder.py
@@ -37,14 +37,16 @@ else:
 #################
 # function to find start position of 1st exon for each gene - takes a GFF file, returns a dict of gene:start position pairs
 def Exon1Finder(GFF,UpstreamLength):
-	Chromosomes = []
 	Genes = []
-	Starts = []
-	Strands = []
+	Chromosomes = {}
+	ExonStarts = {}
+	Strands = {}
+	# go through input, recording gene names in original order and the Chromosome, Strand and Exon Starts for each gene
 	for line in open(GFF,"r"):
 		if not line.startswith("#"):
 			temp = line.split("\t")
 			if temp[2] == "exon":
+				# extract data from GFF fields
 				Chromosome = temp[0]
 				Strand = temp[6]
 				if Strand == "+":
@@ -53,57 +55,59 @@ def Exon1Finder(GFF,UpstreamLength):
 					Start = temp[4]
 				NameMatch = re.search("Parent\=[^\;]*",line)
 				Name = NameMatch.group().replace("Parent=","")
-				print("Currently on an exon for gene " + Name + " starting at " + str(Start))
-				# check whether an exon for this gene has been seen before
-				# if it hasn't been seen before, add all of its information to the lists
-				if Name not in Genes:
-					Chromosomes.append(Chromosome)
-					Genes.append(Name)
-					Starts.append(Start)
-					Strands.append(Strand)
-					print("First site of exon from gene " + Name)
-				# if it has been seen before, compare the start position of this exon to the last element in the Starts list (which will have come from another exon of this gene)
+				# add gene name to list
+				Genes.append(Name)
+				# add Chromosome name to dict if it isn't there already
+				if Name not in Chromosomes:
+					Chromosomes[Name] = Chromosome
+				# add Exon start to dict, either as a new list or appended to the existing list
+				if Name not in ExonStarts:
+					ExonStarts[Name] = [int(Start)]
 				else:
-					# if the gene is on the + strand, replace the last element with this start position if the start position is lower (i.e. more 5' on the + strand)
-					# NB: this should never happen if the exon entries are in ascending numerical order, but keeping the check in here for now just in case
-					if Strand == "+":
-						if Start < Starts[-1]:
-							print("Have already seen exon from gene " + Name + ", but this exon has a more 5' start site (" + str(Start) + " vs " + str(Starts[-1]) + " on the + strand)")
-							del Starts[-1]
-							Starts.append(Start)
-					# if the gene is on the - strand, replace the last element with this start position if the start position is higher (i.e. more 5' on the - strand)
-					if Strand == "-":
-						if Start > Starts[-1]:
-							print("Have already seen exon from gene " + Name + ", but this exon has a more 5' start site (" + str(Start) + " vs " + str(Starts[-1]) + " on the - strand)")
-							del Starts[-1]
-							Starts.append(Start)
+					ExonStarts[Name].append(int(Start))
+				# add strand to dict if it isn't there already
+				if Name not in Strands:
+					Strands[Name] = Strand
+	# sort start coordinates for each gene into ascending order
+	for i in ExonStarts:
+		ExonStarts[i].sort()
 	# calculate upstream region coordinates for each gene
-	UpstreamStarts = []
-	for i in range(len(Starts)):
+	UpstreamStarts = {}
+	for i in Genes:
 		if Strands[i] == "+":
-			UpstreamStarts.append(int(Starts[i])-UpstreamLength-1)
+			UpstreamStarts[i]=ExonStarts[i][0]-UpstreamLength-1
 		elif Strands[i] == "-":
-			UpstreamStarts.append(int(Starts[i])+1)
-	UpstreamEnds = []
-	for i in range(len(Starts)):
+			UpstreamStarts[i]=ExonStarts[i][-1]+1
+	UpstreamEnds = {}
+	for i in Genes:
 		if Strands[i] == "+":
-			UpstreamEnds.append(int(Starts[i])-1)
+			UpstreamEnds[i]=ExonStarts[i][0]-1
 		elif Strands[i] == "-":
-			UpstreamEnds.append(int(Starts[i])+UpstreamLength+1)
+			UpstreamEnds[i]=ExonStarts[i][-1]+UpstreamLength+1
+	# make lists from dicts, preserving original gene input order
+	ChromosomesList = []
+	StrandsList = []
+	UpstreamStartsList = []
+	UpstreamEndsList = []
+	for i in Genes:
+		ChromosomesList.append(Chromosomes[i])
+		StrandsList.append(Strands[i])
+		UpstreamStartsList.append(UpstreamStarts[i])
+		UpstreamEndsList.append(UpstreamEnds[i])
 	# clean up all lists, removing entries where the upstream start coordinate is <1
 	ToDelete = []
-	for i in range(len(UpstreamStarts)):
-		if UpstreamStarts[i]<1:
+	for i in range(len(Genes)):
+		if UpstreamStarts[Genes[i]]<1:
 			ToDelete.append(i)
-	# NB: deleting the elements in reverse order, to ensure that the element numbering isn't disrupted while we're still relying on it
+	# NB: deleting elements from lists in reverse order to preserve element numbering until it doesn't matter
 	for i in reversed(ToDelete):
-		del Chromosomes[i]
-		del UpstreamStarts[i]
-		del UpstreamEnds[i]
-		del Strands[i]
 		del Genes[i]
+		del ChromosomesList[i]
+		del UpstreamStartsList[i]
+		del UpstreamEndsList[i]
+		del StrandsList[i]
 	# make list of lists
-	Combined = list(zip(Chromosomes,UpstreamStarts,UpstreamEnds,Strands,Genes))
+	Combined = list(zip(ChromosomesList,UpstreamStartsList,UpstreamEndsList,StrandsList,Genes))
 	return(Combined)
 
 # final call
