f8a4e98caa14871e1b30f6aaffe9fc0603693721
SamuelHLewis <samuel.h.lewis@gmail.com>
2018-04-09 15:25:31 +0100

    Increased speed by calling MethylExtract on pre-made fasta files

--- MEbatch.sh
+++ MEbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts ":b:c:g:h" opt; do
+while getopts ":b:c:h" opt; do
 	case $opt in
 			b)
 				# set BAM file name
@@ -12,11 +12,6 @@ while getopts ":b:c:g:h" opt; do
 				Contig=$OPTARG
 				printf "Contig (-c): ${Contig}\n"
 				;;
-			g)
-				# set genome fasta file name
-				GenomeFasta=$OPTARG
-				printf "Genome fasta file (-g): ${GenomeFasta}\n"
-				;;
 			h)
 				printf "Valid parameters are:\n-b (BAM file name)\n-c (Contig name)\n-g (genome fasta file name)\n-h display help message\n"
 				exit 1
@@ -24,12 +19,8 @@ while getopts ":b:c:g:h" opt; do
 	esac
 done
 
-# make new directory for this contig
-mkdir ./MEbatch/"${Contig}"
-# grab the name and sequence for this contig from the genome fasta file (NB: genome file must be unwrapped)
-grep -A1 "${Contig}" "${GenomeFasta}" > ./MEbatch/"${Contig}"/"${Contig}".fasta
-# extract the entries for this contig from the bam file, and sort the bam file
-samtools view -h -F 0x4 -q 10 "${BAMfile}" "${Contig}" | samtools view -hbS - > ./MEbatch/"${Contig}"/"${Contig}".bam
+# extract the entries for this contig from the bam file and print to a new bam file
+samtools view -b "${BAMfile}" "${Contig}" > ./MEbatch/"${Contig}"/"${Contig}".bam
 # run MethylExtract on this contig
 ~/bin/MethylExtract_1.9.1/MethylExtract.pl seq=./MEbatch/"${Contig}"/"${Contig}".fasta inDir=./MEbatch/"${Contig}"/ flagW=99,147 flagC=83,163 context=ALL bedOut=Y p=1 chromDiv=800 memNumReads=500000 minDepthMeth=10 chromSplitted=Y
 
