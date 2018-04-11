f5afd8fe7c4decd71d2a71d720d857bd60467a96
SamuelHLewis <samuel.h.lewis@gmail.com>
2018-04-11 12:36:37 +0100

    Added command line argument parsing

--- fasta_splitter.sh
+++ fasta_splitter.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-## split genome fasta file
-#csplit --prefix=seq --digits=6 --silent Genomic.fas '%>%' '/^>/' '{*}'
-## move genome fasta file out of this directory
-#mv Genomic.fas ../
+# command line option parsing
+while getopts ":i:h" opt; do
+	case $opt in
+		i)
+			# set input file
+			printf "input file (-i): $OPTARG\n"
+			InputFile=$OPTARG
+			;;
+		h)
+			# print help message
+			printf "Valid parameters are:\n-i (input fasta file)\n-h (display help message)\n"
+	esac
+done
 
-# for each individual chromosome/contig fasta file...
-for file in $( ls ); do
+# split genome fasta file
+csplit --prefix=seq --digits=6 --silent "${InputFile}" '%>%' '/^>/' '{*}'
+
+# for each individual chromosome/contig fasta file (excluding the input file)...
+for file in $( ls | grep -v -F ${InputFile} ); do
 	# grab the header, strip off the ">", and set this as the Name
 	Name=$( head -n1 $file | cut -d'>' -f2)
 	# make a new directory for this contig
@@ -14,3 +26,6 @@ for file in $( ls ); do
 	# rename the fasta file, and move it into the directory (with the same name)
 	mv $file ./"${Name}"/"${Name}".fasta
 done
+
+printf "Input file split into separate fasta files\n"
+
